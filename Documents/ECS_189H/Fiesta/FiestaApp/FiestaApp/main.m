7ca256aa81aaca2c93897703e073655a68b69691
Meenal <meenal.tambe@gmail.com>
2012-02-11 22:46:50 -0800

    added the menu, created Xcode file

---
+++ Documents/ECS_189H/Fiesta/FiestaApp/FiestaApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FiestaApp
+//
+//  Created by Meenal Tambe on 2/11/12.
+//  Copyright (c) 2012 University of California, Davis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MenuViewAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MenuViewAppDelegate class]));
+    }
+}
